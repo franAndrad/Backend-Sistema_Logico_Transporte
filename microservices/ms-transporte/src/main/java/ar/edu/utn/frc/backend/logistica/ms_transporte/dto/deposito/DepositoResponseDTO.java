757e23739db3248605565c7b19302ca172dcdab0
Francisco Ezequiel Andrade <403499@sistemas.frc.utn.edu.ar>
2025-11-07 07:24:42 -0300

    se adapto cambios del ale

---
+++ microservices/ms-transporte/src/main/java/ar/edu/utn/frc/backend/logistica/ms_transporte/dto/deposito/DepositoResponseDTO.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.backend.logistica.ms_transporte.dto.deposito;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositoResponseDTO {
+    private Integer id;
+    private String mensaje;
+}
